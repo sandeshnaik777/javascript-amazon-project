a993393b2b8733c3fde6f820ffdd2eaba4313f88
sandesh_naik07 <sandeshnaik525@gmail.com>
2025-08-17 22:10:44 +0530

    make delivery option interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart,removeFromCart} from '../data/cart.js';
+import {cart,removeFromCart,updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js'
 import { formatCurreny } from './utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -98,7 +98,8 @@ const dateString = deliveryDate.format('dddd, MMMM D');
 const priceString = deliveryOption.priceCents===0 ? 'Free': `$${formatCurreny(deliveryOption.priceCents)} -`;
    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 html +=
- `<div class="delivery-option">
+ `<div class="delivery-option js-delivery-option " data-product-id="${matchingProduct.id}"
+ data-delivery-option-id="${deliveryOption.id}">
                 <input type="radio"
                 ${isChecked ? 'checked':''}
                 class="delivery-option-input"
@@ -134,3 +135,13 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>
 });
 })
 
+document.querySelectorAll('.js-delivery-option')
+.forEach((element)=>
+{
+  element.addEventListener('click',()=>
+  {
+    const {productId,deliveryOptionId} = element.dataset;
+updateDeliveryOption(productId,deliveryOptionId)
+  }
+)
+});
