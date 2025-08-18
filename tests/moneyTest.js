1bac378151b702c0bc5757d946e2243aca547de0
sandesh_naik07 <sandeshnaik525@gmail.com>
2025-08-18 23:29:14 +0530

    create automated test for formate currency

---
+++ tests/moneyTest.js
@@ -0,0 +1,35 @@
+import formatCurreny from "../scripts/utils/money.js";
+
+console.log('test suite: formatCurrency');
+console.log('converts cents into dollars');
+if(formatCurreny(2095) == 20.95)
+{
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
+
+console.log('works with zero');
+if(formatCurreny(0) == '0.00')
+{
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
+console.log('round up to the nearest cents');
+if(formatCurreny(2000.5) == '20.01')
+{
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
+if(formatCurreny(2000.4) == '20.00')
+{
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
