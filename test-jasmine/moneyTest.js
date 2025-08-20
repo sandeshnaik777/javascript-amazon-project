898d11429bb1644dfa3ffd1f4b81aefae1e7757e
sandesh_naik07 <sandeshnaik525@gmail.com>
2025-08-20 22:45:05 +0530

    Remove default test and add money test

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,18 @@
+import formatCurrency from "../scripts/utils/money.js";
+
+describe('test suite: formateCurrencry', () =>
+{
+    it('converts cents into dollars',()=>
+    {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+
+    it('Works with zero',()=>
+    {
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+     it('round up to the nearest cents',()=>
+    {
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+});
