181a0586a380a70575cbf3ce3be5bef27afb5917
sandesh_naik07 <sandeshnaik525@gmail.com>
2025-08-24 14:17:45 +0530

    Rename test-jasmin folder to tests

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,4 +1,4 @@
-import formatCurreny from "../scripts/utils/money.js";
+import formatCurreny from "../../scripts/utils/money.js";
 
 console.log('test suite: formatCurrency');
 console.log('converts cents into dollars');
